9595494e142c6d059697b4b157f69fc92862f516
Raheem <raheemideadev@gmail.com>
2021-01-06 19:10:46 +0500

    raheem

--- app/src/main/java/xyz/teamgravity/navigationcomponent/fragment/LoginFragment.kt
+++ app/src/main/java/xyz/teamgravity/navigationcomponent/fragment/LoginFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import xyz.teamgravity.navigationcomponent.databinding.FragmentLoginBinding
 
 class LoginFragment: Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private val args by navArgs<LoginFragmentArgs>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
@@ -23,6 +26,8 @@ class LoginFragment: Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         binding.apply {
+            usernameField.setText(args.username)
+
             confirmB.setOnClickListener {
                 val username = usernameField.text.toString()
                 val password = passwordField.text.toString()
