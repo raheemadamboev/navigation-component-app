9e04a8ee2fff7880f04cae41c71d40484aec19de
Raheem <raheemideadev@gmail.com>
2021-01-06 16:40:41 +0500

    raheem

--- app/src/main/java/xyz/teamgravity/navigationcomponent/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/navigationcomponent/MainActivity.kt
@@ -2,10 +2,15 @@ package xyz.teamgravity.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import xyz.teamgravity.navigationcomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
