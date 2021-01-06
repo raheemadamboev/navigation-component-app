530ef529ed30ec16c8bb027e1a6195df78400ff3
Raheem <raheemideadev@gmail.com>
2021-01-06 18:06:19 +0500

    raheem

--- app/src/main/java/xyz/teamgravity/navigationcomponent/activity/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/navigationcomponent/activity/MainActivity.kt
@@ -2,6 +2,10 @@ package xyz.teamgravity.navigationcomponent.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import xyz.teamgravity.navigationcomponent.R
 import xyz.teamgravity.navigationcomponent.databinding.ActivityMainBinding
 
@@ -9,9 +13,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // find nav controller
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    // in order to respond back button in toolbar from fragment
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
